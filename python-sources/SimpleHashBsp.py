f724cecb8d01723ff6be8640c2771d2e664c55f9
Norman Markgraf <nmarkgraf@hotmail.com>
2020-11-17 13:40:26 +0100

    UTF-8 leider nicht in Quellcodes ... :-(

--- python-sources/SimpleHashBsp.py
+++ python-sources/SimpleHashBsp.py
@@ -1,14 +1,15 @@
-def simple_hash(a: str, modulus = 7):
+def simple_hash(txt, modulus=7):
     hash_value = 0
-    for c in a:
+    for c in txt:
         hash_value += (ord(c) - 64) % modulus
     return hash_value % modulus
 
+
 if __name__ == "__main__":
     staedte = ("WIEN", "GRAZ", "BONN", "ULM", "BOCHUM")
-    print("Feldgröße: 7")
+    print("Feldgroesse: 7")
     for stadt in staedte:
         print(f"hash({stadt})={simple_hash(stadt)}")
-    print("-"*25,"\nFeldgröße: 25")
+    print("-"*25,"\nFeldgroesse: 25")
     for stadt in staedte:
         print(f"hash({stadt})={simple_hash(stadt, 25)}")
